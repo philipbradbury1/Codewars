1657e1556f2d82ac11372138900c2a38cfdc8313
Philip <philipbradbury1@gmail.com>
2021-07-01 10:29:22 +0100

    Add some 8 kyu

---
+++ 8 kyu/Count by X.js
@@ -0,0 +1,13 @@
+/*
+Description:
+Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array (or list in Python, Haskell or Elixir).
+
+*/
+
+function countBy(x, n) {
+    return Array.apply(null, Array(n)).map((_, idx) => x + (x * idx));
+}
