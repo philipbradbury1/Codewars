1d2f29d03a11374c650630be7acb7e3325af8999
Philip <philipbradbury1@gmail.com>
2021-08-12 16:18:42 +0100

    added Kyu 7

---
+++ 7 kyu/Smallest Product.js
@@ -0,0 +1,18 @@
+/*
+Description:
+Given a non-empty array of non-empty integer arrays, multiply the contents of each nested array and return the smallest total.
+
+Example
+input = [
+  [1, 5],
+  [2],
+  [-1, -3]
+]
+
+result = 2
+
+*/
+
+function smallestProduct(arr) {
+    return Math.min(...newArr = arr.map(x => x.reduce((a, b) => a * b)))
+}
